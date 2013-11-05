27da71152ef27d950c93422a7f86c3da2caf170a
Chris Price <chris@puppetlabs.com>
2013-11-05 13:28:51 -0800

    Finish adding examples of some more advanced graph stuff

--- src/scratch.clj
+++ src/scratch.clj
@@ -5,6 +5,12 @@
             [plumbing.fnk.pfnk :as pfnk]
             [clojure.pprint :refer [pprint]]))
 
+;; This is a sort of tutorial on some advanced features of the prismatic
+;; graph library; mostly around applying transformations to the service
+;; maps before compiling them to graphs.  If you'd like to play along at home,
+;; the easiest thing to do is probably to fire up a REPL and paste in the
+;; bits of code one section at a time.
+
 ;; First we'll create a couple of services to play with.  Note that I'm putting
 ;; a println in the body of the config service, so that we can see when it gets
 ;; executed.  This is a parallel to the "let"-based startup/initialization code
@@ -14,7 +20,8 @@
   {:config-service
     (fnk []
          (println "Config service initializing!")
-         {:config (fn [k] ({:foo "foo" :bar "bar"} k))})})
+         {:config (fn [k] ({:foo "foo" :bar "bar"} k))
+          :shutdown (fn [] (println "Config service shutting down!"))})})
 
 (defn hello-service
   []
@@ -274,18 +281,157 @@
 ;; Nope, other than the new side-effect printlns, everything works exactly the
 ;; same.
 
+;; OK, so now let's say that we want to abstract away some of the implementation
+;; details of the pfnk/input-schema/output-schema stuff so that we can
+;; accomplish this kind of stuff more easily as we move forward.  The only
+;; part of the `wrap-with-howdy-fnk` that is really meaningful to us right now
+;; is the simple 'f' that we define in the let block.  We're not doing anything
+;; with the schema stuff in this case, so let's assume we can write a higher-order
+;; function to abstract away those details.  We'll call that `fn->fnk-with-same-schema`
+;; and ignore the implementation details for the moment.
+(declare fn->fnk-with-same-schema)
 
+;; So now, assuming we have that available, what would our new version of
+;; the howdy wrapper look like?  (Note I've changed the variable name of 'v'
+;; to 'orig-fnk' to try to clarify things a bit.
+(defn wrap-with-hola-fnk
+  [p orig-fnk]
+  (let [f (fn [injected-vals]
+            (println (format "Hola from '%s'" p))
+            (orig-fnk injected-vals))]
+    (fn->fnk-with-same-schema f orig-fnk)))
 
+;; A little cleaner.  So let's see if we can write that schema handling function:
+(defn fn->fnk-with-same-schema
+  [f orig-fnk]
+  (let [in  (pfnk/input-schema orig-fnk)
+        out (pfnk/output-schema orig-fnk)]
+      (pfnk/fn->fnk f [in out])))
 
+;; Word, let's see if we can run it this way.
+(println "10) testing hola-fnk graph...")
+(let [hola-service-map (walk-leaves-and-path wrap-with-hola-fnk main-service-map)
+      hello-fn (get-in (graph-instance hola-service-map) [:hello-service :hello])]
+  (println (hello-fn)))
 
+;; OUTPUT:
+;10) testing hola-fnk graph...
+;Hola from '[:config-service]'
+;Config service initializing!
+;Hola from '[:hello-service]'
+;Hello, foo!
+;-------------------------------
 
+;; yep.  so now we could stick that fn->fnk-with-same-schema into a library
+;; somewhere and write our wrappers with the simpler syntax.  If we wanted to,
+;; we could use a macro to abstract it even further so that we could just write
+;; something like:
+;;
+;;    (defnkwithsameschema wrap-with-hola
+;;        [p orig-fnk injected-vals]
+;;        (println (format "Hola from '%s'" p))
+;;        (orig-fnk injected-vals))
+;;
+;; OK, so that's all well and good.  But sometimes we actually *do* want to do
+;; things with the schema.  For example, if we're trying to determine whether
+;; or not the various services provide a :shutdown function, we absolutely
+;; need to use the schema to determine that.  Here's a simple wrapper that will
+;; print out whether or not each service has a :shutdown function:
+(defn wrap-with-schema-check-for-shutdown
+  [p orig-fnk]
+  (let [out (pfnk/output-schema orig-fnk)]
+    (if (contains? out :shutdown)
+      (println (format "Service '%s' has a shutdown function!" p)))
+    orig-fnk))
 
+(println "11) simple check for shutdown fn...")
+(walk-leaves-and-path wrap-with-schema-check-for-shutdown main-service-map)
 
+;; OUTPUT:
+;11) simple check for shutdown fn...
+;Service '[:hello-service]' has a shutdown function!
+;Service '[:config-service]' has a shutdown function!
+;-------------------------------
 
+;; Noteworthy there:
+;; * We didn't compile the graph, and the printlns were executed... because we
+;;   didn't wrap the fnk in any way in this example.
+;; * We can detect the presence of the shutdown function via the schema, but
+;;   this example code doesn't actually give us access to the actual shutdown
+;;   function.
+;;
+;; To get access to the actual shutdown functiont, we would have to wrap the fnk,
+;; and in a way slightly differently from what we've done before.
+(defn wrap-with-fnk-check-for-shutdown
+  [p orig-fnk]
+  (let [in (pfnk/input-schema orig-fnk)
+        out (pfnk/output-schema orig-fnk)
+        f (fn [injected-vals]
+            ;; so what we're going to do here is to call the orig-fnk from within
+            ;; a let block.  That gives us access to its service function map
+            ;; from within our code, so we can do some things before we actually
+            ;; return it.
+            (let [result (orig-fnk injected-vals)]
+              ;; for now we'll just print it
+              (println (format "Service graph for '%s':" p))
+              (pprint result)
+              (if-let [shutdown-fn (result :shutdown)]
+                (println "###Service has a shutdown function!:" shutdown-fn))
+              result))]
+    ;; and then we still need to flip our f back to a fnk with the original
+    ;; schemas.
+    (pfnk/fn->fnk f [in out])))
 
+(println "12) check for actual shutdown fn in service map (not just in schema)...")
+(let [wrapped-map (walk-leaves-and-path wrap-with-fnk-check-for-shutdown main-service-map)]
+  (println "Wrapped the map!")
+  (graph-instance wrapped-map))
 
+;; OUTPUT:
+;12) check for actual shutdown fn in service map (not just in schema)...
+;Wrapped the map!
+;Config service initializing!
+;Service graph for '[:config-service]':
+;{:config #<scratch$config_service$pos_fn__1721__auto____4905$fn__4906 scratch$config_service$pos_fn__1721__auto____4905$fn__4906@499375c2>,
+; :shutdown #<scratch$config_service$pos_fn__1721__auto____4905$fn__4908 scratch$config_service$pos_fn__1721__auto____4905$fn__4908@523ed23c>}
+;###Service has a shutdown function!: #<scratch$config_service$pos_fn__1721__auto____4905$fn__4908 scratch$config_service$pos_fn__1721__auto____4905$fn__4908@523ed23c>
+;Service graph for '[:hello-service]':
+;{:hello #<scratch$hello_service$pos_fn__1721__auto____4922$fn__4923 scratch$hello_service$pos_fn__1721__auto____4922$fn__4923@3f3a1398>,
+; :shutdown #<scratch$hello_service$pos_fn__1721__auto____4922$fn__4925 scratch$hello_service$pos_fn__1721__auto____4922$fn__4925@4d04fff>}
+;###Service has a shutdown function!: #<scratch$hello_service$pos_fn__1721__auto____4922$fn__4925 scratch$hello_service$pos_fn__1721__auto____4922$fn__4925@4d04fff>
+;-------------------------------
 
+;; So, you can see a few things from that output:
+;; * The 'Wrapped the map!' println executes first; because our other prints are
+;;   inside of our wrapped fnk, they don't execute until the graph is compiled.
+;; * They have full access to the entire map of service functions for the service.
+;;   This allows us to do things like store a reference to the shutdown function,
+;;   or we could even wrap those inner functions with some other logic if we
+;;   needed to.
+;;
+;; This file is a lot longer than I intended, so I'll just show one more example;
+;; In it, I'll create an atom to use to save references to all of the shutdown
+;; functions, and then call them (in the correct order) after compiling the
+;; graph.
+(defn wrap-with-shutdown-registration
+  [shutdown-fns-atom p orig-fnk]
+  (let [in (pfnk/input-schema orig-fnk)
+        out (pfnk/output-schema orig-fnk)
+        f (fn [injected-vals]
+            (let [result (orig-fnk injected-vals)]
+              (when-let [shutdown-fn (result :shutdown)]
+                (println (format "Registering shutdown function for service '%s'" p))
+                (swap! shutdown-fns-atom conj shutdown-fn))
+              result))]
+    (pfnk/fn->fnk f [in out])))
 
+(let [shutdown-fns (atom ())
+      wrapped-map (walk-leaves-and-path
+                    (partial wrap-with-shutdown-registration shutdown-fns)
+                    main-service-map)
+      g-i         (graph-instance wrapped-map)]
+  (doseq [shutdown-fn @shutdown-fns]
+    (shutdown-fn)))
 
 
 

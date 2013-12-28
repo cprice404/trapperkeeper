7c1e55891203c66050c42c77c6c092382ac29ffd
Chris Price <chris@puppetlabs.com>
2013-12-28 12:08:33 -0800

    Basic "boot" function and initial (failing) tests for lifecycle

--- test/clj/scratch/protocol_apis_test.clj
+++ test/clj/scratch/protocol_apis_test.clj
@@ -1,7 +1,7 @@
 (ns scratch.protocol-apis-test
   (:require [clojure.test :refer :all]
             [plumbing.fnk.pfnk :as pfnk]
-            [scratch.protocol-apis :refer [ServiceLifecycle PrismaticGraphService defservice service service-graph]]))
+            [scratch.protocol-apis :refer [ServiceLifecycle PrismaticGraphService defservice service service-graph boot!]]))
 
 (defprotocol HelloService
   (hello [this msg]))
@@ -47,3 +47,28 @@
                                           :bar-service {:bar identity}})
                hello-fn     (:hello fnk-instance)]
           (is (= "HELLO!: hi" (hello-fn "hi"))))))))
+
+(defprotocol Service1
+  (service1-fn [this]))
+
+(defprotocol Service2
+  (service2-fn [this]))
+
+(deftest lifecycle-test
+  (testing "life cycle functions are called in the correct order"
+    (let [call-seq  (atom [])
+          service1  (service Service1
+                      []
+                      (init [this context] (swap! call-seq conj :init-service1))
+                      (startup [this context] (swap! call-seq conj :startup-service1))
+                      (service1-fn [this] (swap! call-seq conj :service1-fn)))
+          service2  (service Service2
+                      [[:Service1 service1-fn]]
+                      (init [this context] (swap! call-seq conj :init-service2))
+                      (startup [this context] (swap! call-seq conj :startup-service2))
+                      (service2-fn [this] (swap! call-seq conj :service2-fn)))]
+      (is (satisfies? PrismaticGraphService service1))
+      (is (satisfies? PrismaticGraphService service2))
+      (boot! [service1 service2])
+      (is (= [:init-service1 :init-service2 :startup-service1 :startup-service2]
+             @call-seq)))))
